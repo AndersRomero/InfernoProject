ac9fff7777cfa7ef217d1dd1079ececc455d2309
KIMIO1 <estiwengomez0065@gmail.com>
2023-12-20 02:53:35 -0500

    pput

--- proxys/crear.php
+++ proxys/crear.php
@@ -1,25 +1,39 @@
 <?php
-
 include '../conexion.php';
 
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $image = $_POST['image'];
     $description = $_POST['description'];
     $price = $_POST['price'];
 
-    $sql = "INSERT INTO proxy (name, image, description, price) VALUES ('$name', '$image', '$description', '$price')";
-    $query = mysqli_query($conexion, $sql);
-
-    if ($query) {
-        header("Location: index.php");
+    // Validación de campos requeridos
+    if (empty($name) || empty($image) || empty($description) || empty($price)) {
+        echo "Todos los campos son obligatorios.";
+    } elseif (!is_numeric($price)) {
+        echo "El precio debe ser un número.";
     } else {
-        echo "Error al crear el proxy: " . mysqli_error($conexion);
+        // Sentencia preparada para evitar la inyección de SQL
+        $sql = "INSERT INTO proxy (name, image, description, price) VALUES (?, ?, ?, ?)";
+        $stmt = $conexion->prepare($sql);
+
+        // Vincula los parámetros y ejecuta la sentencia
+        $stmt->bind_param("sssd", $name, $image, $description, $price);
+        $query = $stmt->execute();
+
+        if ($query) {
+            header("Location: index.php");
+            exit(); // Importante: salir después de redirigir para evitar la ejecución adicional del código.
+        } else {
+            echo "Error al crear el proxy: " . $stmt->error;
+        }
+
+        // Cierra la declaración
+        $stmt->close();
     }
 }
 
-
-    include '../templates/header.php';
+include '../templates/header.php';
 ?>
 
 <div class="card">
@@ -30,27 +44,25 @@ if ($_POST) {
         <form action="crear.php" enctype="multipart/form-data" method="POST">
             <div class="mb-3">
                 <label for="name" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="name" name="name" aria-describedby="helpId" placeholder="Nombre">
+                <input type="text" class="form-control" id="name" name="name" aria-describedby="helpId" placeholder="Nombre" required>
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Imagen</label>
-                <input type="text" class="form-control" id="image" name="image" aria-describedby="helpId" placeholder="Imagen">
+                <input type="text" class="form-control" id="image" name="image" aria-describedby="helpId" placeholder="Imagen" required>
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descripción</label>
-                <input type="text" class="form-control" id="description" name="description" aria-describedby="helpId" placeholder="Descripción">
+                <input type="text" class="form-control" id="description" name="description" aria-describedby="helpId" placeholder="Descripción" required>
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Precio</label>
-                <input type="text" class="form-control" id="price" name="price" aria-describedby="helpId" placeholder="Precio">
+                <input type="number" class="form-control" id="price" name="price" aria-describedby="helpId" placeholder="Precio" required>
             </div>
 
-            <button type="submit" class="btn btn-success" >Crear</button>
+            <button type="submit" class="btn btn-success">Crear</button>
             <a type="button" class="btn btn-danger" href="index.php">Cancelar</a>
-
+        </form>
     </div>
 </div>
 
-<?php
-include '../templates/footer.php';
-?>
+<?php include '../templates/footer.php'; ?>
--- proxys/editar.php
+++ proxys/editar.php
@@ -1,9 +1,8 @@
-<?php include '../templates/header.php';
-
+<?php
+include '../templates/header.php';
 include '../conexion.php';
 
 if(isset($_GET['txtid'])) {
-
     $txtid = $_GET['txtid'];
     $sentencia = $conexion->prepare("SELECT * FROM proxy WHERE id = ?");
     $resultado = $sentencia->execute([$txtid]);
@@ -17,18 +16,25 @@ if ($_POST) {
     $description = $_POST['description'];
     $price = $_POST['price'];
 
-    $sentencia = $conexion->prepare("UPDATE proxy SET name = ?, image = ?, description = ?, price = ? WHERE id = ?");
-    $resultado = $sentencia->execute([$name, $image, $description, $price, $txtid]);
-
-    if ($resultado === TRUE) {
-        header("Location: index.php");
+    // Validaciones
+    if (empty($name) || empty($image) || empty($description) || empty($price)) {
+        echo "Todos los campos son obligatorios.";
+    } elseif (!is_numeric($price) || $price < 0) {
+        echo "El precio debe ser un número positivo.";
     } else {
-        echo "Error al actualizar el proxy: " . mysqli_error($conexion);
+        // Actualización si las validaciones son exitosas
+        $sentencia = $conexion->prepare("UPDATE proxy SET name = ?, image = ?, description = ?, price = ? WHERE id = ?");
+        $resultado = $sentencia->execute([$name, $image, $description, $price, $txtid]);
+
+        if ($resultado === TRUE) {
+            header("Location: index.php");
+            exit(); // Importante: salir después de redirigir para evitar la ejecución adicional del código.
+        } else {
+            echo "Error al actualizar el proxy: " . mysqli_error($conexion);
+        }
     }
 }
 ?>
-
-
 
 <div class="card">
     <div class="card-header">
@@ -38,29 +44,29 @@ if ($_POST) {
         <form action="editar.php" enctype="multipart/form-data" method="POST">
             <div class="mb-3">
                 <label for="txtid" class="form-label">ID:</label>
-                <input type="text" class="form-control" value="<?php echo $_GET["txtid"]?>" name="txtid" id="txtid" aria-describedby="helpId">
+                <input name="txtid" id="txtid" class="form-control" type="text" value="<?php echo $_GET["txtid"]?>" readonly>
             </div>
 
             <div class="mb-3">
                 <label for="name" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="name" name="name" value="<?php echo $proxy[0]['name']?>" aria-describedby="helpId" placeholder="Nombre">
+                <input type="text" class="form-control" id="name" name="name" value="<?php echo $proxy[0]['name']?>" aria-describedby="helpId" placeholder="Nombre" required>
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Imagen</label>
-                <input type="text" class="form-control" id="image" name="image" value="<?php echo $proxy[0]['image']?>" aria-describedby="helpId" placeholder="Imagen">
+                <input type="text" class="form-control" id="image" name="image" value="<?php echo $proxy[0]['image']?>" aria-describedby="helpId" placeholder="Imagen" required>
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descripción</label>
-                <input type="text" class="form-control" id="description" name="description" value="<?php echo $proxy[0]['description']?>" aria-describedby="helpId" placeholder="Descripción">
+                <input type="text" class="form-control" id="description" name="description" value="<?php echo $proxy[0]['description']?>" aria-describedby="helpId" placeholder="Descripción" required>
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Precio</label>
-                <input type="text" class="form-control" id="price" name="price" value="<?php echo $proxy[0]['price']?>" aria-describedby="helpId" placeholder="Precio">
+                <input type="number" class="form-control" id="price" name="price" value="<?php echo $proxy[0]['price']?>" aria-describedby="helpId" placeholder="Precio" required>
             </div>
 
-            <button type="submit" class="btn btn-success" >Crear</button>
+            <button type="submit" class="btn btn-success">Actualizar</button>
             <a type="button" class="btn btn-danger" href="index.php">Cancelar</a>
-
+        </form>
     </div>
 </div>
 
