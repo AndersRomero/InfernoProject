7745a34c91779845400bdf8916b792e99ba096c4
AnderIS2 <ajromero@jdc.edu.co>
2023-12-26 19:43:27 -0500

    gomezlomama

--- secciones/streaming.php
+++ secciones/streaming.php
@@ -1,10 +1,58 @@
 <?php
 include '../conexion.php';
 
-// Consultar proxy
+// Iniciar o reanudar la sesión para mantener el carrito entre páginas
+session_start();
+
+// Inicializar el carrito si aún no existe
+if (!isset($_SESSION['carrito'])) {
+    $_SESSION['carrito'] = [];
+}
+
+// Consultar streaming
 $sentencia = $conexion->prepare("SELECT * FROM streaming");
 $sentencia->execute();
 $streaming = $sentencia->get_result()->fetch_all(MYSQLI_ASSOC);
+
+// Agregar producto al carrito
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['agregarCarrito'])) {
+    $productoId = $_POST['id'];
+    $productoExistente = array_filter($_SESSION['carrito'], function ($item) use ($productoId) {
+        return $item['id'] == $productoId;
+    });
+
+    // Si el producto ya está en el carrito, mostrar mensaje
+    if (!empty($productoExistente)) {
+        echo "<script>alert('Ya tienes este artículo en tu carrito.')</script>";
+    } else {
+        $producto = [
+            'id' => $_POST['id'],
+            'name' => $_POST['name'],
+            'price' => $_POST['price'],
+            'quantity' => isset($_POST['cantidad']) && $_POST['cantidad'] > 0 ? $_POST['cantidad'] : 1,
+        ];
+
+        // Agregar el producto al carrito
+        array_push($_SESSION['carrito'], $producto);
+    }
+}
+
+// Función para calcular el total del carrito
+function calcularTotal() {
+    $total = 0;
+    foreach ($_SESSION['carrito'] as $item) {
+        $total += $item['price'] * $item['quantity'];
+    }
+    return $total;
+}
+
+// Eliminar producto del carrito
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['eliminarItem'])) {
+    $productoId = $_POST['eliminarItem'];
+    $_SESSION['carrito'] = array_filter($_SESSION['carrito'], function ($item) use ($productoId) {
+        return $item['id'] != $productoId;
+    });
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,6 +71,10 @@ $streaming = $sentencia->get_result()->fetch_all(MYSQLI_ASSOC);
     <link rel="stylesheet" href="../css/secciones.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
+    <style>
+        /* Estilos adicionales para el carrito */
+
+    </style>
 
     <title>INFERNO PROJECT</title>
 </head>
@@ -31,23 +83,32 @@ $streaming = $sentencia->get_result()->fetch_all(MYSQLI_ASSOC);
 <header class="container-fuild animate__animated animate__fadeInDown">
     <nav class="navbar navbar-expand-lg nav-tabs bg-body-tertiary">
         <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="navbar-nav mx-auto  ">
                 <a class="nav-link special" href="streaming.php">Streaming</a>
                 <a class="nav-link special" href="#">Proxys</a>
                 <a class="nav-link special" href="#">Checkers</a>
+                <!-- Icono de Usuario -->
             </div>
-
-            <!-- Icono de administrador -->
             <a href="../admin/login.php" class="admin-icon  ">
                 <i class="fas fa-user-cog"></i>
+
             </a>
+
+            <!-- Icono de carrito -->
+            <span class="carrito-icon" onclick="toggleCarrito()">
+                        <i class="fas fa-shopping-cart"></i>
+                    </span>
+        </div>
         </div>
     </nav>
-
 </header>
-<section>
+
+<section class="text-center">
     </br>
-    <h1 class="text-center text-white animate__animated animate__bounceInLeft">STREAMING</h1>
+    <h1 class="text-white animate__animated animate__bounceInLeft">STREAMING</h1>
     </br>
 
     <div class="container d-flex flex-wrap justify-content-around">
@@ -65,23 +126,95 @@ $streaming = $sentencia->get_result()->fetch_all(MYSQLI_ASSOC);
                             <p class="estado-disponible">Estado: Disponible</p>
                         <?php } ?>
                     </div>
-                    <a href="#" class="btn">Agregar al Carrito</a>
+                    <form method="post" class="form-inline">
+                        <input type="hidden" name="id" value="<?php echo $registros['id']; ?>">
+                        <input type="hidden" name="name" value="<?php echo $registros['name']; ?>">
+                        <input type="hidden" name="price" value="<?php echo $registros['price']; ?>">
+                        <label for="cantidad">Cantidad:</label>
+                        <select name="cantidad" id="cantidad" class="form-control mx-2">
+                            <?php for ($i = 1; $i <= 15; $i++) { ?>
+                                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php } ?>
+                        </select>
+                        <button type="submit" class="btn btn-primary" name="agregarCarrito">Agregar al Carrito</button>
+                    </form>
                 </div>
             </div>
         <?php } ?>
     </div>
 </section>
 
-<!-- Resto del código... -->
+<!-- Carrito -->
+<div class="carrito-container">
+    <div id="carrito" class="carrito">
+        <h3>Tu Carrito</h3>
+        <div class="carrito-items">
+            <?php
+            if (!empty($_SESSION['carrito'])) {
+                foreach ($_SESSION['carrito'] as $item) {
+                    echo "<div class='carrito-item'>";
+                    echo "<p>{$item['name']} ({$item['quantity']})</p>";
+                    echo "<p>Precio: USD$ {$item['price']}</p>";
+                    echo "<form method='post'>";
+                    echo "<input type='hidden' name='eliminarItem' value='{$item['id']}'>";
+                    echo "<button type='submit' class='btn btn-sm btn-danger'>Eliminar</button>";
+                    echo "</form>";
+                    echo "</div>";
+                }
+            } else {
+                echo "<p>El carrito está vacío.</p>";
+            }
+            ?>
+        </div>
+        <?php if (!empty($_SESSION['carrito'])) : ?>
+            <div class="carrito-total">
+                <p>Total: USD $<?php echo calcularTotal(); ?></p>
+                <button onclick="realizarCompra()">Realizar compra</button>
+            </div>
+        <?php endif; ?>
+    </div>
+    <div class="carrito-overlay" onclick="cerrarCarrito()"></div>
+</div>
+
+<!-- JavaScript y Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-6Lla0V7TP1hZl/ZZ9nJ0CmVMWDfXT86a1jww1AEaBAsjFuCZSmKbSSUnQlmh/jpW" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyIdJAgwFdhxjusSzsZgMlCpVLMDma5P6d" crossorigin="anonymous"></script>
 
 <script>
+    function toggleCarrito() {
+        var carrito = document.getElementById("carrito");
+        var overlay = document.querySelector(".carrito-overlay");
+        carrito.style.display = carrito.style.display === "block" ? "none" : "block";
+        overlay.style.display = overlay.style.display === "block" ? "none" : "block";
+    }
+
+    function cerrarCarrito() {
+        var carrito = document.getElementById("carrito");
+        var overlay = document.querySelector(".carrito-overlay");
+        carrito.style.display = "none";
+        overlay.style.display = "none";
+    }
+
+    function realizarCompra() {
+        // Lógica para realizar la compra
+        // Puedes redirigir a una página de pago o realizar otras acciones necesarias
+        alert('Compra realizada. Redirigiendo a la página de pago...');
+    }
+
     function toggleAdminLogin() {
         var adminLoginForm = document.querySelector(".admin-login-form");
         adminLoginForm.style.display =
             adminLoginForm.style.display === "block" ? "none" : "block";
     }
-</script>
 
+
+    window.addEventListener('load', function () {
+        document.querySelectorAll('.card-content form').forEach(function (form) {
+            form.querySelector('#cantidad').value = 1;
+        });
+    });
+</script>
 </body>
 
 </html>
